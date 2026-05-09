812b40698cfc152d53dc29e5a8482bc06509eace
TylerMiller902 <tyler5miller5@gmail.com>
2026-05-08 20:38:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const cors    = require('cors');
 const path    = require('path');
+const fetch   = require('node-fetch');  // needed for Node < 18
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
